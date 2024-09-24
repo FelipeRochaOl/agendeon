a2f454d860e79e9b7237f1818de215fdb8976e0c
Felipe Rocha <feliperochaoliveira@gmail.com>
2024-09-24 06:51:16 -0300

    feat: create urlparams to categories and session
    Signed-off-by: Felipe Rocha <feliperochaoliveira@gmail.com>

--- src/main/java/br/com/agendaon/category/CategoryPresenter.java
+++ src/main/java/br/com/agendaon/category/CategoryPresenter.java
@@ -3,6 +3,7 @@ package br.com.agendaon.category;
 import br.com.agendaon.session.SessionModel;
 import lombok.Data;
 
+import java.text.Normalizer;
 import java.util.UUID;
 
 @Data
@@ -10,10 +11,21 @@ public class CategoryPresenter {
     private UUID code;
     private SessionModel session;
     private String name;
+    private String urlPath;
 
     CategoryPresenter(CategoryModel categoryModel) {
         this.code = categoryModel.getCode();
         this.session = categoryModel.getSession();
         this.name = categoryModel.getName();
+        this.setUrlPath();
+    }
+
+    private void setUrlPath() {
+        String name = this.removeAccents(this.name);
+        this.urlPath = name.toLowerCase().replaceAll("[^0-9a-zA-Z]+", "-");
+    }
+
+    private String removeAccents(String str) {
+        return Normalizer.normalize(str, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
     }
 }
